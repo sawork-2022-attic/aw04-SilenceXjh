f53c29122a69331420862637f30b5bbb029b8662
xjh <xjh@xjhdeMacBook-Pro.local>
2022-04-05 10:34:01 +0800

    code

--- src/main/java/com/example/webpos/web/PosController.java
+++ src/main/java/com/example/webpos/web/PosController.java
@@ -9,18 +9,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @Controller
 public class PosController {
 
+    @Autowired
+    private HttpSession session;
+
     private PosService posService;
 
-    private Cart cart;
+    private Cart getCart() {
+        Cart cart = (Cart)session.getAttribute("cart");
+        if(cart == null) {
+            cart = new Cart();
+            this.saveCart(cart);
+        }
+        return cart;
+    }
 
-    @Autowired
-    public void setCart(Cart cart) {
-        this.cart = cart;
+    private void saveCart(Cart cart) {
+        session.setAttribute("cart", cart);
     }
 
     @Autowired
@@ -29,17 +39,18 @@ public class PosController {
     }
 
     @GetMapping("/")
-    public String pos(Model model) {
+    public String pos(Model model, HttpServletRequest request) {
+        request.getSession(true);
         model.addAttribute("products", posService.products());
-        model.addAttribute("cart", cart);
+        model.addAttribute("cart", getCart());
         return "index";
     }
 
     @GetMapping("/add")
     public String addByGet(@RequestParam(name = "pid") String pid, Model model) {
-        posService.add(cart, pid, 1);
+        saveCart(posService.add(getCart(), pid, 1));
         model.addAttribute("products", posService.products());
-        model.addAttribute("cart", cart);
+        model.addAttribute("cart", getCart());
         return "index";
     }
 }
